50c7a4c0952c547e86c1617084c96d1dc6cfc16b
Oleg Zhmelev <99668293+losdayver@users.noreply.github.com>
2022-05-23 17:15:44 +0300

    Добавил возможность осматриваться правой кнопкой мыши/пробелом

--- main.py
+++ main.py
@@ -14,12 +14,14 @@ from drawer import *
 counter = 0
 for y in [2*i for i in range(5)]:
     for x in [2*i for i in range(5)]:
-        tile.Tile.place_tile(
+        game_board.Tile.place_tile(
             location=(x, y),
             rotation=0)
-        tile.Tile.selected_tile = tile.Tile.tiles_pile[counter]
+        game_board.Tile.selected_tile = game_board.Tile.tiles_pile[counter]
         counter+=1
 
+mouse_lock = False
+mouse_lock_location = [0,0]
 can_press_mouse = True
 clock = pg.time.Clock()
 while 1:
@@ -35,15 +37,29 @@ while 1:
     if keys_pressed[pg.K_LEFT]: VIEW_PORT_CENTRE[0] -= VIEW_PORT_MOV_SPEED
 
     mouse_pressed = pg.mouse.get_pressed(3)
+    if mouse_pressed[2] or keys_pressed[pg.K_SPACE]:
+        if not mouse_lock:
+            mouse_lock = True
+            mouse_lock_location = pg.mouse.get_pos()
+        else:
+
+            VIEW_PORT_CENTRE[0] += mouse_lock_location[0] - pg.mouse.get_pos()[0]
+            VIEW_PORT_CENTRE[1] += mouse_lock_location[1] - pg.mouse.get_pos()[1]
+            mouse_lock_location = pg.mouse.get_pos()
+    else: mouse_lock = False
+
+    print(mouse_lock_location)
+
+
     if mouse_pressed[0] and can_press_mouse:
 
         can_press_mouse = False
-        tile.Tile.place_tile(
+        game_board.Tile.place_tile(
             location=((pg.mouse.get_pos()[0] + VIEW_PORT_CENTRE[0]) // GRID_SCALE,
                       (pg.mouse.get_pos()[1] + VIEW_PORT_CENTRE[1]) // GRID_SCALE),
             rotation=random.randint(0,4))
 
-        tile.Tile.selected_tile = random.choice(tile.Tile.tiles_pile)
+        game_board.Tile.selected_tile = random.choice(game_board.Tile.tiles_pile)
 
     elif not mouse_pressed[0]: can_press_mouse = True
 
--- settings.py
+++ settings.py
@@ -1,9 +1,9 @@
 import pygame as pg
 import numpy as np
 
-FPS = 30
+FPS = 60
 GRID_SCALE = 78
 pg.init()
-SCREEN = pg.display.set_mode([1280, 720])
+SCREEN = pg.display.set_mode([1280, 720], pg.RESIZABLE)
 VIEW_PORT_CENTRE = [0, 0]
 VIEW_PORT_MOV_SPEED = 8
