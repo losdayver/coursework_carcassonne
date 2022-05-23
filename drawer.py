878b80cb27f68082576310be08748cac63bed42a
Oleg Zhmelev <99668293+losdayver@users.noreply.github.com>
2022-05-23 17:15:14 +0300

    Тайлы обрезаются по краям для более комфортного восприятия

--- drawer.py
+++ drawer.py
@@ -1,17 +1,20 @@
 import math
 
+import pygame
 import pygame as pg
+from settings import *
 import game_board
 
 from game_board import *
 from settings import *
 
-def draw_board():
+def draw_board(crop=2):
     for t in Tile.tiles_pile:
         for l in t.placements:
-            SCREEN.blit(source=pg.transform.rotate(t.sprite, l['rotation'] * 90),
-                        dest=[l['location'][0] * GRID_SCALE - VIEW_PORT_CENTRE[0],
-                              l['location'][1] * GRID_SCALE - VIEW_PORT_CENTRE[1]])
+            SCREEN.blit(source=pg.transform.rotate(t.sprite, l['rotation'] * 90).subsurface([
+                crop,crop,t.sprite.get_width()-crop*2,t.sprite.get_height()-crop*2]),
+                        dest=[l['location'][0] * GRID_SCALE - VIEW_PORT_CENTRE[0] + crop,
+                              l['location'][1] * GRID_SCALE - VIEW_PORT_CENTRE[1] + crop])
 
 def draw_debug_info():
     for t in Tile.tiles_pile:
@@ -50,6 +53,10 @@ def draw_debug_info():
                     pg.draw.lines(SCREEN, color, False, vertex_array, 2)
 
 def draw_gui():
-    SCREEN.blit(pg.transform.scale(tile.Tile.selected_tile.sprite, [40, 40]), [10, 10])
+    SCREEN.fill([255,255,255], [0, 0, 100, SCREEN.get_rect()[3]])
+    SCREEN.blit(pg.transform.scale(game_board.Tile.selected_tile.sprite, [60, 60]), [20, 20])
+
+def draw_tile_highlight(location):
+    SCREEN.blit(game_board.Tile.selected_tile.sprite, location)
 
 
