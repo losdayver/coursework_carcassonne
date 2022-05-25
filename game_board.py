458f4c3f18121420c9f7bd5db76394b1af865e58
Oleg Zhmelev <99668293+losdayver@users.noreply.github.com>
2022-05-25 15:50:08 +0300

    Добавлена проверка на возможность поставить тайл в требуемую позицию

--- game_board.py
+++ game_board.py
@@ -1,26 +1,60 @@
 from settings import *
+import random
 
 # Данный класс описывает тип тайла, а не отдельные сущности тайлов
 class Tile:
+    total_amount = 72
     tiles_pile = [] # Список всех возможных видов тайлов
-    selected_tile = None # Выбранный в данный момент тайл, который будет участвовать в следующем хрде
+    selected_tile = None # Выбранный в данный момент тайл, который будет участвовать в следующем ходе
+    selected_tile_rotation = 0 # Вращение выбранного тайла
 
     def __init__(self, sprite:pg.Surface, quantity:int, connections:list=[], has_monastery:bool=False, has_shield:bool=False):
-        self.quantity = quantity # Количество тайлов данного типа в колоде (осталось)
-        self.connections = connections # Здесь прописаны поля, дороги, города на тайле
-        # Оформление имеет вид { 'type' : 'town', 'connections' : [True, False, False, True]}
+        # Количество тайлов данного типа в колоде (осталось)
+        self.quantity = quantity
 
+        # Здесь прописаны поля, дороги, города на тайле, а также их соединения
+        self.connections = connections # { 'type' : 'town', 'connections' : [True, False, False, True]}
+
+        # Данный список хранит какие типы соединений имеет тайл со всех сторон без информации о соединениях
+        self.simlified_connections = ['field', 'field', 'field', 'field']
+
+        for connection in self.connections:
+            for i, con in enumerate(connection['connections']):
+                if con: self.simlified_connections[i] = connection['type']
+
+        # Есть ли монастырь?
         self.has_monastery = has_monastery
-        self.has_shield = has_shield
-        self.sprite = sprite
-        self.index = len(Tile.tiles_pile) # Индекс тайла в списке всех видов тайлов tiles_pile
 
+        # Есть ли щит?
+        self.has_shield = has_shield
+
+        # Картинка тайла из папки ./Resources
+        self.sprite = sprite
+
+        # Индекс тайла в списке tiles_pile
+        self.index = len(Tile.tiles_pile)
+
+        # Список координат и вращений тайлов этого типа на доске
         self.placements = [] # { location : (x,y), rotation : r }
 
         Tile.tiles_pile.append(self)
 
     @staticmethod
-    def place_tile(location, rotation):
+    def pick_random_tile():
+        random_number = random.randint(0, Tile.total_amount)
+        counter = 0
+
+        for t in Tile.tiles_pile:
+            counter += t.quantity
+            if counter > random_number:
+                Tile.selected_tile = t
+                return
+
+    @staticmethod
+    def place_tile(location, test_if_can_be_placed = True):
+        if test_if_can_be_placed and not Tile.can_place_tile(location): return
+
+        print(Tile.selected_tile.simlified_connections)
         is_place_occupied = False
         print(location)
 
@@ -29,122 +63,156 @@ class Tile:
                 if l['location'] == location: return
 
         Tile.selected_tile.quantity -= 1
-        Tile.selected_tile.placements.append({'location' : location, 'rotation' : rotation})
+        Tile.total_amount -= 1
+        Tile.selected_tile.placements.append({'location' : location, 'rotation' : Tile.selected_tile_rotation})
+
+        Tile.pick_random_tile()
+
+    @staticmethod
+    def can_place_tile(location):
+        has_at_least_one_connection = False
+
+        for t in Tile.tiles_pile:
+            for p in t.placements:
+
+                # Проверяемый тайл выше текущего
+                if p['location'][0] == location[0] and p['location'][1] == location[1] - 1:
+                    has_at_least_one_connection = True
+                    if not t.simlified_connections[(2 + p['rotation']) % 4] == \
+                    Tile.selected_tile.simlified_connections[(0 + Tile.selected_tile_rotation) % 4]:
+                        return False
+
+                # Проверяемый тайл ниже текущего
+                elif p['location'][0] == location[0] and p['location'][1] == location[1] + 1:
+                    has_at_least_one_connection = True
+                    if not t.simlified_connections[(0 + p['rotation']) % 4] == \
+                    Tile.selected_tile.simlified_connections[(2 + Tile.selected_tile_rotation) % 4]:
+                        return False
+
+                # Проверяемый тайл правее текущего
+                elif p['location'][0] == location[0] + 1 and p['location'][1] == location[1]:
+                    has_at_least_one_connection = True
+                    if not t.simlified_connections[(3 + p['rotation']) % 4] == \
+                    Tile.selected_tile.simlified_connections[(1 + Tile.selected_tile_rotation) % 4]:
+                        return False
+
+                # Проверяемый тайл левее текущего
+                elif p['location'][0] == location[0] - 1 and p['location'][1] == location[1]:
+                    has_at_least_one_connection = True
+                    if not t.simlified_connections[(1 + p['rotation']) % 4] == \
+                    Tile.selected_tile.simlified_connections[(3 + Tile.selected_tile_rotation) % 4]:
+                        return False
 
 
-
-
-
-
+        return has_at_least_one_connection
 
 # Здесь описаны все возможные виды тайлов в оригинальной игре
-
-Tile.selected_tile = Tile(quantity=4,
+Tile(quantity=4,
     connections=[],
     has_monastery = True,
     sprite = pg.image.load('./resources/tile-b.png'))
 
-Tile.selected_tile = Tile(quantity=2,
+Tile(quantity=2,
     connections=[
         {'type' : 'road', 'connections' : [False, False, True, False]}],
     has_monastery = True,
     sprite = pg.image.load('./resources/tile-a.png'))
 
-Tile.selected_tile = Tile(quantity=1,
+Tile(quantity=1,
     connections=[
         {'type' : 'town', 'connections' : [True, True, True, True]}],
     has_shield = True,
     sprite = pg.image.load('./resources/tile-c.png'))
 
-Tile.selected_tile = Tile(quantity=3,
+Tile(quantity=3,
     connections=[
         {'type' : 'town', 'connections' : [True, True, False, True]}],
     sprite = pg.image.load('./resources/tile-r.png'))
 
-Tile.selected_tile = Tile(quantity=1,
+Tile(quantity=1,
     connections=[
         {'type' : 'town', 'connections' : [True, True, False, True]}],
     has_shield=True,
     sprite = pg.image.load('./resources/tile-q.png'))
 
-Tile.selected_tile = Tile(quantity=1,
+Tile(quantity=1,
     connections=[
         {'type' : 'town', 'connections' : [True, True, False, True]},
         {'type' : 'road', 'connections' : [False, False, True, False]}],
     sprite = pg.image.load('./resources/tile-t.png'))
 
-Tile.selected_tile = Tile(quantity=2,
+Tile(quantity=2,
     connections=[
         {'type' : 'town', 'connections' : [True, True, False, True]},
         {'type' : 'road', 'connections' : [False, False, True, False]}],
     has_shield=True,
     sprite = pg.image.load('./resources/tile-s.png'))
 
-Tile.selected_tile = Tile(quantity=3,
+Tile(quantity=3,
     connections=[
         {'type' : 'town', 'connections' : [True, False, False, True]}],
     sprite = pg.image.load('./resources/tile-n.png'))
 
-Tile.selected_tile = Tile(quantity=2,
+Tile(quantity=2,
     connections=[
         {'type' : 'town', 'connections' : [True, False, False, True]}],
     has_shield=True,
     sprite = pg.image.load('./resources/tile-m.png'))
 
-Tile.selected_tile = Tile(quantity=3,
+Tile(quantity=3,
     connections=[
         {'type' : 'town', 'connections' : [True, False, False, True]},
         {'type' : 'road', 'connections' : [False, True, True, False]}],
     sprite = pg.image.load('./resources/tile-p.png'))
 
-Tile.selected_tile = Tile(quantity=2,
+Tile(quantity=2,
     connections=[
         { 'type' : 'town', 'connections' : [True, False, False, True]},
         { 'type' : 'road', 'connections' : [False, True, True, False]}],
     has_shield=True,
     sprite=pg.image.load('./resources/tile-o.png'))
 
-Tile.selected_tile = Tile(quantity=1,
+Tile(quantity=1,
     connections=[
         { 'type' : 'town', 'connections' : [False, True, False, True]}],
     sprite = pg.image.load('./resources/tile-g.png'))
 
-Tile.selected_tile = Tile(quantity=2,
+Tile(quantity=2,
     connections=[
         { 'type' : 'town', 'connections' : [False, True, False, True]}],
     has_shield=True,
     sprite = pg.image.load('./resources/tile-f.png'))
 
-Tile.selected_tile = Tile(quantity=2,
+Tile(quantity=2,
     connections=[
         { 'type' : 'town', 'connections' : [True, False, False, False]},
         { 'type' : 'town', 'connections' : [False, False, False, True]}],
     sprite = pg.image.load('./resources/tile-i.png'))
 
-Tile.selected_tile = Tile(quantity=3,
+Tile(quantity=3,
     connections=[
         { 'type' : 'town', 'connections' : [True, False, False, False]},
         { 'type' : 'town', 'connections' : [False, False, True, False]}],
     sprite = pg.image.load('./resources/tile-h.png'))
 
-Tile.selected_tile = Tile(quantity=5,
+Tile(quantity=5,
     connections=[
         { 'type' : 'town', 'connections' : [True, False, False, False]}],
     sprite = pg.image.load('./resources/tile-e.png'))
 
-Tile.selected_tile = Tile(quantity=3,
+Tile(quantity=3,
     connections=[
         { 'type' : 'town', 'connections' : [True, False, False, False]},
         { 'type' : 'road', 'connections' : [False, False, True, True]}],
     sprite = pg.image.load('./resources/tile-k.png'))
 
-Tile.selected_tile = Tile(quantity=3,
+Tile(quantity=3,
     connections=[
         { 'type' : 'town', 'connections' : [True, False, False, False]},
         { 'type' : 'road', 'connections' : [False, True, True, False]}],
     sprite = pg.image.load('./resources/tile-j.png'))
 
-Tile.selected_tile = Tile(quantity=3,
+Tile(quantity=3,
     connections=[
         { 'type' : 'town', 'connections' : [True, False, False, False]},
         { 'type' : 'road', 'connections' : [False, True, False, False]},
@@ -152,30 +220,30 @@ Tile.selected_tile = Tile(quantity=3,
         { 'type' : 'road', 'connections' : [False, False, False, True]}],
     sprite = pg.image.load('./resources/tile-l.png'))
 
-Tile.selected_tile = Tile(quantity=3,
+Tile(quantity=3,
     connections=[
         { 'type' : 'town', 'connections' : [True, False, False, False]},
         { 'type' : 'road', 'connections' : [False, True, False, True]}],
     sprite = pg.image.load('./resources/tile-d.png'))
 
-Tile.selected_tile = Tile(quantity=8,
+Tile(quantity=8,
     connections=[
         { 'type' : 'road', 'connections' : [True, False, True, False]}],
     sprite = pg.image.load('./resources/tile-u.png'))
 
-Tile.selected_tile = Tile(quantity=9,
+Tile(quantity=9,
     connections=[
         { 'type' : 'road', 'connections' : [False, False, True, True]}],
     sprite = pg.image.load('./resources/tile-v.png'))
 
-Tile.selected_tile = Tile(quantity=4,
+Tile(quantity=4,
     connections=[
         { 'type' : 'road', 'connections' : [False, True, False, False]},
         { 'type' : 'road', 'connections' : [False, False, True, False]},
         { 'type' : 'road', 'connections' : [False, False, False, True]}],
     sprite = pg.image.load('./resources/tile-w.png'))
 
-Tile.selected_tile = Tile(quantity=1,
+Tile(quantity=1,
     connections=[
         { 'type' : 'road', 'connections' : [True, False, False, False]},
         { 'type' : 'road', 'connections' : [False, True, False, False]},
@@ -184,7 +252,7 @@ Tile.selected_tile = Tile(quantity=1,
     sprite = pg.image.load('./resources/tile-x.png'))
 
 # Starting tile
-Tile.selected_tile = Tile(quantity=3,
+Tile.selected_tile = Tile(quantity=1,
     connections=[
         { 'type' : 'town', 'connections' : [True, False, False, False]},
         { 'type' : 'road', 'connections' : [False, True, False, True]}],
--- main.py
+++ main.py
@@ -1,76 +1,59 @@
-#TODO
-# 1 день -- сделал расстановку тайлов 1 типа
-# 2 день -- сделал визуальный дебаг
-
 if __name__ != '__main__': quit()
 
-from settings import *
 import random
-
+from settings import *
 import game_board
-
 from drawer import *
-
-counter = 0
-# for y in [2*i for i in range(5)]:
-#     for x in [2*i for i in range(5)]:
-#         game_board.Tile.place_tile(
-#             location=(x, y),
-#             rotation=0)
-#         game_board.Tile.selected_tile = game_board.Tile.tiles_pile[counter]
-#         counter+=1
 
 mouse_lock = False
 mouse_lock_location = [0,0]
 can_press_mouse = True
+can_press_rotate = True
 clock = pg.time.Clock()
+
+game_board.selected_tile = game_board.Tile.tiles_pile[-1]
+game_board.Tile.place_tile([5, 5], False)
+
 while 1:
     event = pg.event.get()
     for e in event:
         if pg.QUIT == e.type: quit()
 
     keys_pressed = pg.key.get_pressed()
-
-    if keys_pressed[pg.K_UP]: VIEW_PORT_CENTRE[1] -= VIEW_PORT_MOV_SPEED
-    if keys_pressed[pg.K_DOWN]: VIEW_PORT_CENTRE[1] += VIEW_PORT_MOV_SPEED
-    if keys_pressed[pg.K_RIGHT]: VIEW_PORT_CENTRE[0] += VIEW_PORT_MOV_SPEED
-    if keys_pressed[pg.K_LEFT]: VIEW_PORT_CENTRE[0] -= VIEW_PORT_MOV_SPEED
-
     mouse_pressed = pg.mouse.get_pressed(3)
+
     if mouse_pressed[2] or keys_pressed[pg.K_SPACE]:
         if not mouse_lock:
             mouse_lock = True
             mouse_lock_location = pg.mouse.get_pos()
         else:
-
             VIEW_PORT_CENTRE[0] += mouse_lock_location[0] - pg.mouse.get_pos()[0]
             VIEW_PORT_CENTRE[1] += mouse_lock_location[1] - pg.mouse.get_pos()[1]
             mouse_lock_location = pg.mouse.get_pos()
     else: mouse_lock = False
 
-    print(mouse_lock_location)
-
+    if keys_pressed[pg.K_r] and can_press_rotate:
+        can_press_rotate = False
+        game_board.Tile.selected_tile_rotation += 1
+        game_board.Tile.selected_tile_rotation %= 4
+    elif not keys_pressed[pg.K_r]: can_press_rotate = True
 
     if mouse_pressed[0] and can_press_mouse:
-
         can_press_mouse = False
         game_board.Tile.place_tile(
             location=((pg.mouse.get_pos()[0] + VIEW_PORT_CENTRE[0]) // GRID_SCALE,
-                      (pg.mouse.get_pos()[1] + VIEW_PORT_CENTRE[1]) // GRID_SCALE),
-            rotation=0)
-
-        game_board.Tile.selected_tile = random.choice(game_board.Tile.tiles_pile)
+                      (pg.mouse.get_pos()[1] + VIEW_PORT_CENTRE[1]) // GRID_SCALE),)
 
     elif not mouse_pressed[0]: can_press_mouse = True
 
-    SCREEN.fill([0,0,0])
+    SCREEN.fill([255,255,255])
     draw_board()
 
     draw_debug_info()
 
-    draw_gui()
     draw_tile_highlight(location=((pg.mouse.get_pos()[0] + VIEW_PORT_CENTRE[0]) // GRID_SCALE * GRID_SCALE - VIEW_PORT_CENTRE[0],
                                   ((pg.mouse.get_pos()[1] + VIEW_PORT_CENTRE[1]) // GRID_SCALE * GRID_SCALE - VIEW_PORT_CENTRE[1])))
+    draw_gui()
 
     pg.display.flip()
 
